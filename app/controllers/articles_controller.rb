ac3a45fb6098ebf8440b5d01bdf3f3363a18931c
Narayan <narayangupta763@gmail.com>
2017-02-12 19:42:04 +0000

    Added show action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,15 +4,15 @@ class ArticlesController < ApplicationController
     end 
 
     def create 
-      #  render plain: params[:article].inspect
         @article = Article.new(article_params)
         @article.save
-      #  redirect_to article_path(@article)
     end 
-    
+    def show
+        @article = Article.find(params[:id])
+    end 
     private 
     def article_params
-      params.require(:article).permit(:title, :description)
-   end
+        params.require(:article).permit(:title, :description)
+    end
 
 end 
