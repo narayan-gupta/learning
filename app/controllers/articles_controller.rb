2e303d5ca9e3a64b341ac9b2bc322af7c2bf6754
Narayan <narayangupta763@gmail.com>
2017-02-14 01:20:08 +0000

    Added listings

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,14 +2,44 @@ class ArticlesController < ApplicationController
     def new 
         @article = Article.new
     end 
-
+    
+    def index 
+        @articles = Article.all
+    end 
+    
+    def edit
+        @article = Article.find(params[:id])
+    end 
+    
     def create 
         @article = Article.new(article_params)
-        @article.save
+        if @article.save
+            flash[:notice] = "Article was created"
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
     end 
+    
     def show
         @article = Article.find(params[:id])
     end 
+    
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was Updated"
+            redirect_to article_path(@article) #This is the show action path and it takes in a article 
+        else 
+            render "edit"
+        end 
+    end
+    
+    
+    
+    
+    
+    
     private 
     def article_params
         params.require(:article).permit(:title, :description)
